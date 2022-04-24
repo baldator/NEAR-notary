d3764ac33f61ebaf0c9e41b14b7bafb0e61fecd4
balda <marcotorello@gmail.com>
2022-04-24 23:35:02 +0200

    smart contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,8 +1,126 @@
 // @nearfile
-import { Tip, ReturnObject, ReturnTip, tips, receiverIdx, senderIdx, trackIdx, accountToId, idToAccount, trackOwner } from './model';
-import { context, ContractPromiseBatch, PersistentVector, u128, storage } from "near-sdk-as";
+import { Document, UserSignature, documentIdx, documents, accountToId, idToAccount } from './model';
+import { context, storage } from "near-sdk-as";
 
 
+export function addDocument(hash: string, witness: string | null, users: string[] | null): ReturnObject<Document | null> | null {
+
+  const senderId = _accountToId(context.sender);
+  const witnessId = witness ? _accountToId(witness) : 0;
+  const usersId = users ? users.map(user => _accountToId(user)) : [];
+  let documentInd = storage.getPrimitive<u32>('dc', 0);
+
+  const document = new Document(witnessId, usersId, senderId, hash);
+  documents.set(documentInd, document);
+
+  // add document to user documentIdx
+  for (let i = 0; i < usersId.length; i++) {
+    let userId = usersId[i];
+    let userDocIdx = documentIdx.get(userId, []);
+    userDocIdx.push(documentInd);
+    documentIdx.set(userId, userDocIdx);
+  }
+
+  // add document to witness documentIdx
+  if (witnessId) {
+    let witnessDocIdx = documentIdx.get(witnessId, []);
+    witnessDocIdx.push(documentInd);
+    documentIdx.set(witnessId, witnessDocIdx);
+  }
+
+  // add document to sender documentIdx
+  let senderDocIdx = documentIdx.get(senderId, []);
+  senderDocIdx.push(documentInd);
+  documentIdx.set(senderId, senderDocIdx);
+
+  // update document index
+  documentInd++;
+  return {
+    success: true,
+    error_code: '',
+    error_message: '',
+    data: document
+  };
+}
+
+// Sign a document
+export function signDocument(documentId: u32, user: string): ReturnObject<UserSignature | null> | null {
+  const userId = _accountToId(user);
+  const document = _getDocument(documentId);
+  if (!document) {
+    return {
+      success: false,
+      error_code: '',
+      error_message: 'Document not found',
+      data: null
+    };
+  }
+  // check if user can sign
+  if (!_userCanSign(documentId, userId)) {
+    return {
+      success: false,
+      error_code: '',
+      error_message: 'User cannot sign',
+      data: null
+    };
+  }
+
+
+  // check if user is already signed
+  let userSignature = document.u.find(us => us.u == userId);
+  if (userSignature) {
+    return {
+      success: false,
+      error_code: '',
+      error_message: 'User already signed',
+      data: null
+    };
+  }
+
+  userSignature = new UserSignature(userId);
+  userSignature.signDocument();
+  document.u.push(userSignature);
+  documents.set(documentId, document);
+  return {
+    success: true,
+    error_code: '',
+    error_message: '',
+    data: userSignature
+  };
+}
+
+// Getters
+export function getDocument(id: u32): ReturnObject<Document | null> | null {
+  const document = _getDocument(id);
+  return {
+    success: document != null,
+    error_code: '',
+    error_message: '',
+    data: document
+  };
+}
+
+export function getDocumentsByUserId(userId: u32): ReturnObject<Array<Document>> | null {
+  const userDocIdx = documentIdx.get(userId, []);
+  const documents = userDocIdx.map(id => getDocument(id));
+  return {
+    success: true,
+    error_code: '',
+    error_message: '',
+    data: documents
+  };
+}
+
+// Check if all users have signed, that means UserSignature c is set
+export function isDocumentComplete(documentId: u32): boolean {
+  const document = _getDocument(documentId);
+  if (!document) {
+    return false;
+  }
+  return document.u.every(us => us.c != 0) && document.w.c != 0;
+}
+
+// Private methods
 
 function _accountToId(account: string): u32 {
   assert(account != '', "Account is blank");
@@ -11,7 +129,7 @@ function _accountToId(account: string): u32 {
     return accountToId.getSome(account);
   }
 
-  let userCount = storage.getPrimitive<u32>('uc', 0);
+  let userCount = storage.getPrimitive<u32>('uc', 1);
   userCount++;
   accountToId.set(account, u32(userCount));
   idToAccount.set(u32(userCount), account);
@@ -22,4 +140,27 @@ function _accountToId(account: string): u32 {
 function _idToAccount(id: u32): string {
   const account = idToAccount.get(id);
   return account ? account : '';    
+}
+
+function _getDocument(documentId: u32): Document | null {
+  const document = documents.get(documentId);
+  return document;
+}
+
+// check if userId is creator, users or witness
+function _userCanSign(documentId, userId): boolean {
+  const document = _getDocument(documentId);
+  if (!document) {
+    return false;
+  }
+  if (document.s == userId) {
+    return true;
+  }
+  if (document.u.find(us => us.u == userId)) {
+    return true;
+  }
+  if (document.w == userId) {
+    return true;
+  }
+  return false;
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -9,17 +9,22 @@ const initDate = 1640995200000000000;
 export class Document {  // class written to chain as JSON, hence the short variable names
   w: UserSignature; // witness
   u: Array<UserSignature>; // users id
-  s: string; // sender
+  s: u32; // sender
   c: u32; // date created
-  constructor(withness: u32, users: Array<u32>) {
+  d: string; // ipfs hash
+  constructor(withness: u32, users: Array<u32>, sender: u32, hash: string) {
     this.w = new UserSignature(withness);
-    this.s = context.predecessor;
+    this.s = sender;
+    this.d = hash;
     this.c = u32((context.blockTimestamp - initDate) / 10 ** 9);
     this.u = [];
     for (var _i = 0; _i < users.length; _i++) {
         let user = new UserSignature(users[_i]);
         this.u.push(user);
     }
+    // create user signature for sender
+    let senderSig = new UserSignature(sender);
+    this.u.push(senderSig);
   }
 }
 
@@ -46,19 +51,7 @@ export class UserSignature {  // only used for return data
   data: T;
 }
 
-export const accountToId = new PersistentMap<string, u32>("ai");
-export const idToAccount = new PersistentMap<u32, string>("ia");
-export const tips        = new PersistentMap<u32, Tip>("t"); // 1 {1, 2, 11} // 2 {2, 2, 11}
-export const receiverIdx = new PersistentMap<u32, Array<u32>>("rx"); // reciever1 [1,2]
-export const senderIdx   = new PersistentMap<u32, Array<u32>>("sx"); // sender1 [1] // sender2 [2]
-export const trackIdx    = new PersistentMap<u32, Array<u32>>("tx"); // 12 (trackid)  [1,2]
-export const trackOwner  = new PersistentMap<u32, u32>("to"); // 1 2 - user 1 owns track 2
-
-/**
- * collections.vector is a persistent collection. Any changes to it will
- * be automatically saved in the storage.
- * The parameter to the constructor needs to be unique across a single contract.
- * It will be used as a prefix to all keys required to store data in the storage.
- */
-//export const tips = new PersistentMap<string, TrackTips>("");
-//export const receivers = new PersistentMap<string, PersistentVector<Tip>>("r");
+export const accountToId = new PersistentMap<string, u32>("i");
+export const idToAccount = new PersistentMap<u32, string>("a");
+export const documents   = new PersistentMap<u32, Document>("d"); // 1 {1, 2, 11} // 2 {2, 2, 11}
+export const documentIdx = new PersistentMap<u32, Array<u32>>("m"); // user1 [1,2]
